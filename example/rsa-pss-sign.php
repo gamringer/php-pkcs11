a597f374103fa6d965aae98ad551bf722159f78d
Guillaume Amringer <gamringer@carillon.ca>
2020-04-15 22:36:38 -0400

    Adding target test for PSS

---
+++ example/rsa-pss-sign.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+$module = new Pkcs11\Module('/usr/lib/softhsm/libsofthsm2.so');
+$slotList = $module->getSlotList();
+$session = $module->openSession($slotList[0], Pkcs11\CKF_RW_SESSION);
+$session->login(Pkcs11\CKU_USER,'123456');
+
+$keypair = $session->generateKeyPair(Pkcs11\CKM_RSA_PKCS_KEY_PAIR_GEN, [
+	Pkcs11\CKA_VERIFY => true,
+	Pkcs11\CKA_MODULUS_BITS => 2048,
+	Pkcs11\CKA_PUBLIC_EXPONENT => hex2bin('010001'),
+],[
+	Pkcs11\CKA_TOKEN => true,
+	Pkcs11\CKA_PRIVATE => true,
+	Pkcs11\CKA_SENSITIVE => true,
+	Pkcs11\CKA_SIGN => true,
+]);
+
+$data = "Hello World!";
+$signature = $keypair->pkey->sign(
+	Pkcs11\CKM_SHA256_RSA_PKCS_PSS,
+	$data,
+	new Pkcs11\RsaPssParams(Pkcs11\CKM_SHA256, Pkcs11\CKG_MGF1_SHA256, 32)
+);
+
+var_dump(bin2hex($signature));
+
+$session->logout();
