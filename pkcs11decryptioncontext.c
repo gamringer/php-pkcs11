ab2ac9ed24b257fa37a6cdc8822622d4353fdee7
Guillaume Amringer <g.amringer@gmail.com>
2026-03-04 14:40:03 -0500

    Adjusting file headers to reflect actual project name, copyright and license

--- pkcs11decryptioncontext.c
+++ pkcs11decryptioncontext.c
@@ -1,16 +1,12 @@
 /*
    +----------------------------------------------------------------------+
-   | PHP Version 7                                                        |
+   | PHP PKCS11                                                           |
    +----------------------------------------------------------------------+
-   | Copyright (c) The PHP Group                                          |
+   | Copyright (c) Guillaume Amringer                                     |
    +----------------------------------------------------------------------+
-   | This source file is subject to version 3.01 of the PHP license,      |
-   | that is bundled with this package in the file LICENSE, and is        |
-   | available through the world-wide-web at the following url:           |
-   | http://www.php.net/license/3_01.txt                                  |
-   | If you did not receive a copy of the PHP license and are unable to   |
-   | obtain it through the world-wide-web, please send a note to          |
-   | license@php.net so we can mail you a copy immediately.               |
+   | This source file is subject to the MIT license, that is bundled with |
+   | this package in the file LICENSE, and is available through the at    |
+   | the following url: https://mit-license.org/                          |
    +----------------------------------------------------------------------+
    | Author: Guillaume Amringer                                           |
    +----------------------------------------------------------------------+
